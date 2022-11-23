83c42d664afc52382cd8d907d9d27142e45178b5
raccioly20 <74776017+raccioly20@users.noreply.github.com>
2022-11-23 09:38:59 -0300

    at2311

--- gerarpdf2.R
+++ gerarpdf2.R
@@ -1,7 +1,8 @@
-xaringan_to_pdf("https://ime05-12547.netlify.app/_site/content/slides03/index.html#1")
+xaringan_to_pdf("https://ime05-12547.netlify.app/_site/content/slides04/index.html#1")
 
 
 remotes::install_github("jhelvy/xaringanBuilder")
 library(xaringanBuilder)
+setwd("E:/OneDrive/GitHub/IME05-12532/content/slides06")
 build_pdf("index.Rmd")
 build_pdf("index.html")               
